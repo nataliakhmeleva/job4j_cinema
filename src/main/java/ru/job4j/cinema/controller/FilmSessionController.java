610c321c5759abdad4598c3d8e8a5eec997ed3f8
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-02-09 14:09:34 +0300

    Реализована страница с расписанием

---
+++ src/main/java/ru/job4j/cinema/controller/FilmSessionController.java
@@ -0,0 +1,25 @@
+package ru.job4j.cinema.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.cinema.service.FilmSessionService;
+
+@ThreadSafe
+@Controller
+@RequestMapping("/filmSessions")
+public class FilmSessionController {
+    private final FilmSessionService filmSessionService;
+
+    public FilmSessionController(FilmSessionService filmSessionService) {
+        this.filmSessionService = filmSessionService;
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("filmSessions", filmSessionService.findAll());
+        return "filmSessions/list";
+    }
+}
